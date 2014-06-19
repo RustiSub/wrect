91b8367e219714391f2a95a5062ae4c3f74c43dd
Benno Daenen <bda@anaxis.be>
2014-06-19 13:42:04 +0200

    Fixed issue of stats being stored by ref on anything inherited from MovableEntity.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     var game = new Game();
-    var player = new Player('player', {x: 200, y: 200}, 'resources/player.png', true, {speed: 5});
+    var player = new Player('player', {x: 200, y: 200}, 'resources/player.png', true, {speed: 5, health: 5});
+    var otherSprite = new MovableEntity('other', {x: 400, y: 200}, 'resources/player.png', true, {speed: 1, health: 1});
 
     game.getEntityManager().addEntity(player);
+    game.getEntityManager().addEntity(otherSprite);
 };
