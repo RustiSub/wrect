52384e91afacec936b3afa494f9259cbaae4c281
wouter pirotte <wpi@anaxis.be>
2014-09-26 14:03:21 +0200

    [Shield]: Shield rotates with public api.

--- js/Entities/Force.js
+++ js/Entities/Force.js
@@ -4,12 +4,33 @@
  */
 var Force = BaseEntity.extend({
 
-  //TODO : Move to Shield
-  origin: 0,
-  begin: 0,
-  end: 0,
+  params: {},
 
-  /**
+  drawCone: function (forceGraphics, params) {
+    forceGraphics.beginFill(0xFFFFFF);
+    forceGraphics.moveTo(params.origin.x, params.origin.y);
+    forceGraphics.lineTo(params.begin.x, params.begin.y);
+    forceGraphics.lineTo(params.top.x, params.top.y);
+    forceGraphics.lineTo(params.end.x, params.end.y);
+    forceGraphics.lineTo(params.origin.x, params.origin.y);
+    forceGraphics.endFill();
+  },
+  createCone: function (origin, length, forceAngle, angle) {
+    var params = {};
+    params.length= length;
+    params.forceAngle = forceAngle;
+    params.angle = angle;
+    params.origin = origin;
+    params.begin = new Vector(origin.x + length, params.origin.y);
+    forceAngle = window.game.getHelpers().math.toRadians(forceAngle);
+    params.begin = params.begin.rotate(forceAngle, origin);
+
+    angle = window.game.getHelpers().math.toRadians(angle);
+    params.end = params.begin.rotate(angle, params.origin);
+    params.top = params.begin.add(params.end).scale(0.51);
+
+    return params;
+  }, /**
    *
    * @param {Vector}origin
    * @param {Number}length
@@ -17,34 +38,20 @@ var Force = BaseEntity.extend({
    * @param {Number}forceAngle
    */
   init: function(origin, length, angle, forceAngle) {
-    var forceGraphics = new PIXI.Graphics();
-    this.origin = origin;
-    this.begin = new Vector(origin.x + length, this.origin.y);
-    forceAngle = window.game.getHelpers().math.toRadians(forceAngle);
-    this.begin = this.begin.rotate(forceAngle, origin);
+    this.params = this.createCone(origin, length, forceAngle, angle);
 
-    angle = window.game.getHelpers().math.toRadians(angle);
-    this.end = this.begin.rotate(angle, this.origin);
-    this.top = this.begin.add(this.end).scale(0.51);
-
-    forceGraphics.beginFill(0xFFFFFF);
-    forceGraphics.moveTo(this.origin.x, this.origin.y);
-    forceGraphics.lineTo(this.begin.x, this.begin.y);
-    forceGraphics.lineTo(this.top.x, this.top.y);
-    forceGraphics.lineTo(this.end.x, this.end.y);
-    forceGraphics.lineTo(this.origin.x, this.origin.y);
-    forceGraphics.endFill();
-
-    this._super('shield', forceGraphics);
+    var graphics = new PIXI.Graphics();
+    this.drawCone(graphics, this.params);
+    this._super('shield', graphics);
 
     this.dimensions = {};
 
-    this.dimensions.topLeft = this.origin;
-    this.dimensions.topRight = this.begin;
-    this.dimensions.bottomRight = this.top;
-    this.dimensions.bottomLeft = this.end;
+    this.dimensions.topLeft = this.params.origin;
+    this.dimensions.topRight = this.params.begin;
+    this.dimensions.bottomRight = this.params.top;
+    this.dimensions.bottomLeft = this.params.end;
 
-    this.dimensions.vertices = function(dimensions) {
+    this.dimensions.vertices = function (dimensions) {
       return [
         dimensions.topLeft,
         dimensions.topRight,
@@ -54,6 +61,12 @@ var Force = BaseEntity.extend({
     };
 
     this.physicsBody = {};
+  },
+  adjustWidth: function(angle) {
+    this._graphics.clear();
+    this.params.forceAngle = angle;
+    this.params = this.createCone(this.params.origin, this.params.length, this.params.forceAngle, this.params.angle);
+    this.drawCone(this._graphics, this.params);
   },
   update: function() {
 
