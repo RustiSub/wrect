b9fe8b7ae2e2690256a27fecc924899c2312be64
RustiSub <wpi.anaxis@gmail.com>
2014-10-31 23:13:34 +0100

    [Refactor]: #ECS: Euler again till I figure what's up and down. (stuff kinda works)

--- js/ECS/System/Mover.js
+++ js/ECS/System/Mover.js
@@ -32,45 +32,18 @@
     if (entity.components.RigidBody && !entity.components.RigidBody.frozen) {
       var rigidBody = entity.components.RigidBody;
 
-      var fy = 0;
-      var newPosition = new Vector(0, 0);
+      var acceleration = rigidBody.physicsBody.a.x / 1;
+      rigidBody.physicsBody.v.x += acceleration * dt;
+      var position = rigidBody.physicsBody.v.x * dt;
 
-      /* Verlet integration for the y-direction */
-      var dy = rigidBody.physicsBody.v.y * dt + (0.5 * rigidBody.physicsBody.a.y * dt * dt);
+      var newPosition = new Vector(position, 0);
 
-      newPosition.y += dy;
-      var new_ay = fy;
-      var avg_ay = 0.5 * (new_ay + rigidBody.physicsBody.a.y);
-      rigidBody.physicsBody.v.y += avg_ay * dt;
-
-      console.log(rigidBody.physicsBody.v.y);
-
-      //xi+1 = xi + (xi - xi-1) * (dti / dti-1) + a * dti * dti
-      //var pi_0 = rigidBody.dimensions.previousOrigin || rigidBody.dimensions.origin;
-      //var xi_0 = pi_0.x;
-      //var xi = rigidBody.dimensions.origin.x;
-      //var xi_1 = xi + (xi - xi_0) * (delta2) + rigidBody.physicsBody.a.x * delta * delta;
-      //
-      //console.log(xi_1);
-
-      //var last_acceleration = rigidBody.physicsBody.a;
-      //var modifier = Math.pow(delta, 2) * 0.5;
-      //var newPosition = rigidBody.physicsBody.v.multiply(delta).add(last_acceleration.multiply(modifier));
-      //var new_acceleration = rigidBody.physicsBody.a;//force / mass
-      //var avg_acceleration = ( last_acceleration.add(new_acceleration)).divide(2);
-
-      //rigidBody.physicsBody.v = rigidBody.physicsBody.v.add(avg_acceleration.multiply(delta));
-
-      rigidBody.dimensions.move(rigidBody.physicsBody.v);
-
-      //rigidBody.dimensions.previousOrigin = rigidBody.dimensions.origin;
+      rigidBody.dimensions.move(newPosition);
 
       if (entity.components.Visual) {
         var visual = entity.components.Visual;
 
-        var ceilNewPosition = new Vector(Math.ceil(rigidBody.physicsBody.x), Math.ceil(rigidBody.physicsBody.y));
-
-        var graphicPositionVector = new Vector(visual.graphics.position.x, visual.graphics.position.y).add(ceilNewPosition);
+        var graphicPositionVector = new Vector(visual.graphics.position.x, visual.graphics.position.y).add(newPosition);
         visual.graphics.position.x = graphicPositionVector.x;
         visual.graphics.position.y = graphicPositionVector.y;
       }
