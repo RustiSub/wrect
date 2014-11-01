20c5f9ae8dfcb1cf5fa19812fc6a56f559911b40
RustiSub <wpi.anaxis@gmail.com>
2014-11-01 16:03:59 +0100

    [Refactor]: #Physics: Now with 100% less console log.

--- js/ECS/System/Gravity.js
+++ js/ECS/System/Gravity.js
@@ -26,7 +26,6 @@
       var rigidBody = entity.components.RigidBody;
 
       rigidBody.physicsBody.a = rigidBody.physicsBody.a.add(new wrect.Physics.Vector(0, 9.81));
-      console.log(rigidBody.physicsBody.a);
     }
   }
 }());
