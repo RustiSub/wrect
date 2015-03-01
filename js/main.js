7220cb494003396bed17b6a7f58b6fb262c75d6b
WOUTER PIROTTE <wpi.anaxis@gmail.com>
2015-03-01 21:33:14 +0100

    [Circle Physics]: Setup stage to rotate rectangle. Need rotate function that rotates dimension and any visual component. Also need redraw function again.

--- js/main.js
+++ js/main.js
@@ -60,6 +60,8 @@ window.onload = function() {
     function createJumpBlock(options) {
       var materialBlock = createBlock(options);
       materialBlock.addComponent(new wrect.ECS.Component.BaseMaterial({absorb: new wrect.Physics.Vector(0, 0)}));
+
+      return materialBlock;
     }
 
     function createBlock(options) {
@@ -88,7 +90,7 @@ window.onload = function() {
     var ball = createBall({
       x: 300,
       y: 100,
-      radius: 50,
+      radius: 25,
       color: 0xFFFFFF
     });
 
@@ -100,27 +102,27 @@ window.onload = function() {
     //  color: 0xFFFFFF
     //});
 
-    ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(100, 20));
+    ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(0, 20));
 
-    createJumpBlock({
-      x: 10,
-      y: 10,
-      w: 1000,
-      h: 5,
-      color: 0x000000,
-      alpha: 1
-    });
+    //createJumpBlock({
+    //  x: 10,
+    //  y: 10,
+    //  w: 1000,
+    //  h: 5,
+    //  color: 0x000000,
+    //  alpha: 1
+    //});
 
-    createJumpBlock({
-      x: 1005,
-      y: 10,
-      w: 5,
-      h: 495,
-      color: 0x000000,
-      alpha: 1
-    });
+    //createJumpBlock({
+    //  x: 1005,
+    //  y: 10,
+    //  w: 5,
+    //  h: 495,
+    //  color: 0x000000,
+    //  alpha: 1
+    //});
 
-    createJumpBlock({
+    var block = createJumpBlock({
       x: 10,
       y: 500,
       w: 1000,
@@ -129,14 +131,15 @@ window.onload = function() {
       alpha: 1
     });
 
-    createJumpBlock({
-      x: 10,
-      y: 10,
-      w: 5,
-      h: 495,
-      color: 0x000000,
-      alpha: 1
-    });
+    //
+    //createJumpBlock({
+    //  x: 10,
+    //  y: 10,
+    //  w: 5,
+    //  h: 495,
+    //  color: 0x000000,
+    //  alpha: 1
+    //});
 
   });
 };
