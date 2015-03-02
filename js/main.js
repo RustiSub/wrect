9360e7a469ba1fa6a6936af25cadaeb3aaace866
WOUTER PIROTTE <wpi.anaxis@gmail.com>
2015-03-02 22:33:38 +0100

    [Tilemap]: #CollisionLayer: Setup stuff to fix offset of collion layer physicsbody.

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ window.onload = function() {
 
     function createJumpBlock(options) {
       var materialBlock = createBlock(options);
-      materialBlock.addComponent(new wrect.ECS.Component.BaseMaterial({absorb: new wrect.Physics.Vector(0, 0)}));
+      //materialBlock.addComponent(new wrect.ECS.Component.BaseMaterial({absorb: new wrect.Physics.Vector(0.01, 0.9)}));
 
       return materialBlock;
     }
@@ -87,96 +87,66 @@ window.onload = function() {
     }
 
     var ball = createBall({
-      x: 300,
-      y: 100,
+      x: 1200,
+      y: 200,
       radius: 25,
       color: 0xFFFFFF
     });
 
-    //var topLeft = createPolygon({
-    //  origin: new wrect.Physics.Vector(10, 20),
-    //  vertices: [
-    //    new wrect.Physics.Vector(10, 50),
-    //    new wrect.Physics.Vector(100, 50),
-    //    new wrect.Physics.Vector(50, 200),
-    //    new wrect.Physics.Vector(10, 160)
-    //  ],
-    //  color: 0xFFFFFF
-    //});
-    //
-    //var topRight = createPolygon({
-    //  origin: new wrect.Physics.Vector(500, 20),
-    //  vertices: [
-    //    new wrect.Physics.Vector(10, 50),
-    //    new wrect.Physics.Vector(100, 50),
-    //    new wrect.Physics.Vector(80, 200),
-    //    new wrect.Physics.Vector(50, 160)
-    //  ],
-    //  color: 0xFFFFFF
-    //});
-    //
-    //var bottomRight = createPolygon({
-    //  origin: new wrect.Physics.Vector(500, 300),
-    //  vertices: [
-    //    new wrect.Physics.Vector(10, 50),
-    //    new wrect.Physics.Vector(100, 50),
-    //    new wrect.Physics.Vector(80, 200),
-    //    new wrect.Physics.Vector(-50, 160)
-    //  ],
-    //  color: 0xFFFFFF
-    //});
-    //
-    //var bottomLeft = createPolygon({
-    //  origin: new wrect.Physics.Vector(80, 250),
-    //  vertices: [
-    //    new wrect.Physics.Vector(10, 50),
-    //    new wrect.Physics.Vector(100, 50),
-    //    new wrect.Physics.Vector(80, 200),
-    //    new wrect.Physics.Vector(-50, 160)
-    //  ],
-    //  color: 0xFFFFFF
-    //});
-    //
-    //ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(15, 50));
-    //
-    //createJumpBlock({
-    //  x: 10,
-    //  y: 10,
-    //  w: 1000,
-    //  h: 5,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
-    //
-    //createJumpBlock({
-    //  x: 1005,
-    //  y: 10,
-    //  w: 5,
-    //  h: 495,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
-    //
-    //var block = createJumpBlock({
-    //  x: 10,
-    //  y: 500,
-    //  w: 1000,
-    //  h: 5,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
-    //
-    //
-    //createJumpBlock({
-    //  x: 10,
-    //  y: 10,
-    //  w: 5,
-    //  h: 495,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
+    ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(30, 0));
+
+      createJumpBlock({
+      x: 0,
+      y: 0,
+      w: 1500,
+      h: 5,
+      color: 0x000000,
+      alpha: 1
+    });
+
+    createJumpBlock({
+      x: 1500,
+      y: 0,
+      w: 5,
+      h: 600,
+      color: 0x000000,
+      alpha: 1
+    });
+
+    createJumpBlock({
+      x: 0,
+      y: 600,
+      w: 1500,
+      h: 5,
+      color: 0x000000,
+      alpha: 1
+    });
+
+    createJumpBlock({
+      x: 0,
+      y: 0,
+      w: 5,
+      h: 600,
+      color: 0x000000,
+      alpha: 1
+    });
 
     wrect.getGame()._tileMapManager.loadMap('resources/levels/tilemap/kitchen.json');
 
+
+    //var player = createBlock({
+    //  x: 100,
+    //  y: 100,
+    //  w: 100,
+    //  h: 150,
+    //  color: 0xFFFFFF,
+    //  useSprite: true,
+    //  alpha: 0
+    //});
+    //
+    //player.components.RigidBody.frozen = false;
+    //player.components.RigidBody.gravity = false;
+    //
+    //player.addComponent(new wrect.ECS.Component.ControlScheme.Player());
   });
 };
