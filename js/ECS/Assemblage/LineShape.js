68d6d33af5bff35a7d5eb403e6e2c2fa867c6937
Wouter Pirotte <wouter.pirotte@saga.be>
2015-03-02 13:12:18 +0100

    [Prototype]: #Polygon: Add polygon and lineshape to allow 4 pointed shapes that interact with the physics engine.

---
+++ js/ECS/Assemblage/LineShape.js
@@ -0,0 +1,39 @@
+(function() {
+  "use strict";
+
+  wrect.ECS = wrect.ECS || {};
+  wrect.ECS.Assemblage = wrect.ECS.Assemblage || {};
+
+  var Entity = wrect.ECS.Entity;
+  var Vector = wrect.Physics.Vector;
+  var Polygon = wrect.Geometry.Polygon;
+
+  wrect.ECS.Assemblage.LineShape = function (options) {
+    this.entity = new Entity();
+
+    var rigidBody = new wrect.ECS.Component.RigidBody({
+      dimensions: new Polygon({
+        vertices: options.vertices
+      })
+    });
+
+    var visualComponent = new wrect.ECS.Component.Visual({
+      color: options.color,
+      alpha: options.alpha,
+      graphics: new PIXI.Graphics(),
+      useSprite: options.useSprite,
+      shape: rigidBody.dimensions
+    });
+
+    visualComponent.draw(visualComponent.graphics, visualComponent.options);
+
+    this.entity.addComponent(rigidBody);
+    this.entity.addComponent(visualComponent);
+    //entity.addComponent( new ECS.Components.Position());
+    //entity.addComponent( new ECS.Components.Collision());
+
+    rigidBody.dimensions.move(options.origin || new Vector(0, 0));
+
+    return this.entity;
+  };
+}());
--- js/main.js
+++ js/main.js
@@ -65,13 +65,6 @@ window.onload = function() {
     }
 
     function createBlock(options) {
-      options = options || {
-        x: 300,
-        y: 100,
-        w: 100,
-        h: 40,
-        color: 0xFFFFFF
-      };
       var block = new wrect.ECS.Assemblage.Block(options);
 
       game.getEntityManager().addEntity(block);
@@ -81,10 +74,16 @@ window.onload = function() {
 
     function createBall(options) {
       var ball = new wrect.ECS.Assemblage.Ball(options);
-
       game.getEntityManager().addEntity(ball);
 
       return ball;
+    }
+
+    function createPolygon(options) {
+      var polygon = new wrect.ECS.Assemblage.LineShape(options);
+      game.getEntityManager().addEntity(polygon);
+
+      return polygon;
     }
 
     var ball = createBall({
@@ -94,33 +93,69 @@ window.onload = function() {
       color: 0xFFFFFF
     });
 
-    //var block = createBlock({
-    //  x: 300,
-    //  y: 250,
-    //  w: 100,
-    //  h: 40,
-    //  color: 0xFFFFFF
-    //});
+    var topLeft = createPolygon({
+      origin: new wrect.Physics.Vector(10, 20),
+      vertices: [
+        new wrect.Physics.Vector(10, 50),
+        new wrect.Physics.Vector(100, 50),
+        new wrect.Physics.Vector(50, 200),
+        new wrect.Physics.Vector(10, 160)
+      ],
+      color: 0xFFFFFF
+    });
 
-    ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(0, 20));
+    var topRight = createPolygon({
+      origin: new wrect.Physics.Vector(500, 20),
+      vertices: [
+        new wrect.Physics.Vector(10, 50),
+        new wrect.Physics.Vector(100, 50),
+        new wrect.Physics.Vector(80, 200),
+        new wrect.Physics.Vector(50, 160)
+      ],
+      color: 0xFFFFFF
+    });
 
-    //createJumpBlock({
-    //  x: 10,
-    //  y: 10,
-    //  w: 1000,
-    //  h: 5,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
+    var bottomRight = createPolygon({
+      origin: new wrect.Physics.Vector(500, 300),
+      vertices: [
+        new wrect.Physics.Vector(10, 50),
+        new wrect.Physics.Vector(100, 50),
+        new wrect.Physics.Vector(80, 200),
+        new wrect.Physics.Vector(-50, 160)
+      ],
+      color: 0xFFFFFF
+    });
 
-    //createJumpBlock({
-    //  x: 1005,
-    //  y: 10,
-    //  w: 5,
-    //  h: 495,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
+    var bottomLeft = createPolygon({
+      origin: new wrect.Physics.Vector(80, 250),
+      vertices: [
+        new wrect.Physics.Vector(10, 50),
+        new wrect.Physics.Vector(100, 50),
+        new wrect.Physics.Vector(80, 200),
+        new wrect.Physics.Vector(-50, 160)
+      ],
+      color: 0xFFFFFF
+    });
+
+    ball.components.RigidBody.physicsBody.f = ball.components.RigidBody.physicsBody.f.add(new wrect.Physics.Vector(15, 50));
+
+    createJumpBlock({
+      x: 10,
+      y: 10,
+      w: 1000,
+      h: 5,
+      color: 0x000000,
+      alpha: 1
+    });
+
+    createJumpBlock({
+      x: 1005,
+      y: 10,
+      w: 5,
+      h: 495,
+      color: 0x000000,
+      alpha: 1
+    });
 
     var block = createJumpBlock({
       x: 10,
@@ -131,15 +166,15 @@ window.onload = function() {
       alpha: 1
     });
 
-    //
-    //createJumpBlock({
-    //  x: 10,
-    //  y: 10,
-    //  w: 5,
-    //  h: 495,
-    //  color: 0x000000,
-    //  alpha: 1
-    //});
+
+    createJumpBlock({
+      x: 10,
+      y: 10,
+      w: 5,
+      h: 495,
+      color: 0x000000,
+      alpha: 1
+    });
 
   });
 };
