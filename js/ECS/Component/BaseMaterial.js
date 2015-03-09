7fd9616834d21d72af721a73cbdb870fd297432b
WOUTER PIROTTE <wpi.anaxis@gmail.com>
2015-03-09 23:27:09 +0100

    [Physics]: #Surface: Fix absorbing of gravity.

--- js/ECS/Component/BaseMaterial.js
+++ js/ECS/Component/BaseMaterial.js
@@ -10,7 +10,7 @@
     wrect.ECS.Component.BaseComponent.call(this);
 
     this.options = options || {};
-    this.bounce = options.bounce || 1/1.5;
+    this.bounce = options.bounce || 1;
     this.bounceFriction =  options.bounceFriction || 0;
     this.friction = options.friction || 1;
 
@@ -20,18 +20,24 @@
       }
 
       var perpSurface = data.surface.perpendicular();
-      var forceProjection = data.force.dot(perpSurface);
-      var sign = forceProjection >= 0 ? 1 : -1;
+      var forcePerpProjection = data.force.dot(perpSurface);
+      var sign = forcePerpProjection >= 0 ? 1 : -1;
 
       //Absorb gravity
       var gravityForce = perpSurface.unitScalar(sign * data.otherEntity.components.RigidBody.physicsBody.a.y);
+      data.force = data.force.subtract(gravityForce);
+      //gravityForce = data.surface.unitScalar(sign * data.otherEntity.components.RigidBody.physicsBody.a.x);
       //data.force = data.force.subtract(gravityForce);
 
       //Absorb perpendicular movement
-      var perpForce = data.surface.perpendicular().unitScalar(forceProjection * this.bounce);
+      var perpForce = data.surface.perpendicular().unitScalar(forcePerpProjection * this.bounce);
       data.force = data.force.subtract(perpForce);
 
       //Absorb parallel movement
+      var forceParProjection = data.force.dot(data.surface);
+      var parForce = data.surface.unitScalar(forceParProjection * this.bounce);
+      data.force = data.force.subtract(parForce);
+
       var parProjection = data.force.dot(data.surface);
       var surfaceFrictionSign = parProjection > 0 ? 1 : -1;
       var surfaceFriction = this.friction * surfaceFrictionSign;
@@ -42,9 +48,10 @@
         parProjection = surfaceFriction;
       }
 
-      var parForce = data.surface.unitScalar(parProjection);
-      data.force = data.force.subtract(parForce);
+      var frictionForce = data.surface.unitScalar(parProjection);
+      data.force = data.force.subtract(frictionForce);
 
+      data.Force = new Vector(0, 0);
     }, this);
   };
 
