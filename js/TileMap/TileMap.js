1fdfe885f5e37ce5284ccd7581bb6b986a39c88c
Wouter Pirotte <wouter.pirotte@saga.be>
2015-03-09 12:56:26 +0100

    [Prototype]: #Movin!: Setup new kitchen tileset.

--- js/TileMap/TileMap.js
+++ js/TileMap/TileMap.js
@@ -209,6 +209,12 @@
       }
       tile = changeableTiles.splice(0, 1)[0];
 
+      if (!tile) {
+        //console.log(tile);
+        //debugger;
+        continue;
+      }
+
       var xPosition = (tilePosition % this.width) * tile.width;
       var yPosition = Math.floor(tilePosition / this.width) * tile.height;
 
